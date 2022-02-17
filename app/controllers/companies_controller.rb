f2b27fb63803ee208a94fd06b299e1720f6dbf90
giulzacc <giuliana.zaccardelli@kellogg.northwestern.edu>
2022-02-17 02:23:23 +0000

    class

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -9,4 +9,15 @@ class CompaniesController < ApplicationController
     @company = Company.find(params["id"])
   end
 
+  def new
+    @company = Company.new
+  end
+
+  def create
+    # values =  params["company"]
+    @company = Company.new(params["company"])
+    @company.save
+  end
+
+
 end
